04c2b446f8528559a3ea57efec90a1a4f3d8b933
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-01-10 20:36:08 -0500

    fixed a small syntactical error in index.html that was preventing the button to the notes.html page from working. Finally got everything connected and working the way it is supposed to

--- instructions/Develop/routes/htmlRoutes.js
+++ instructions/Develop/routes/htmlRoutes.js
@@ -2,11 +2,15 @@ var path = require("path")
 var router = require("express").Router();
 
 router.get(`*`, function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   
+//you need to change each of these to be looked for under develop --> public.
+//the one from line for is "index.html"
+//the one below is "notes.html"
+
 router.get(`/notes`, function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
   module.exports = router;
